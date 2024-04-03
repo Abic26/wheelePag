7471ce4dd3c04cb2f5796dec502250e3b58be474
Abic26 <abicsupa@gmail.com>
2024-04-02 22:31:20 -0500

    que pasas 2

--- src/app/components/utilities/sectionPrice.jsx
+++ src/app/components/utilities/sectionPrice.jsx
@@ -1,6 +1,17 @@
 // "use client";
 export const SectionPrice = () => {
-  return <div>
-    <p>¡Descarga la hoja de vida del equipo haciendo <a className="opacity-55 hover:text-gray-400 border-b" href="ruta/a/tu/archivo.pdf" download="Hoja_de_vida_equipo.pdf">clic aquí!</a></p>
-  </div>;
+  return (
+    <div>
+      <p>
+        ¡Descarga la hoja de vida del equipo haciendo{" "}
+        <a
+          className="opacity-55 hover:text-gray-400 border-b"
+          href="HDV_EQUIPOS_.pdf"
+          download
+        >
+          clic aquí!
+        </a>
+      </p>
+    </div>
+  );
 };
